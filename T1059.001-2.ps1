408825c65793378f5810fe97ae387bbe928fd408
AMIKA <60911813+AMIKA-pl@users.noreply.github.com>
2022-09-23 05:28:14 -0400

    Add files via upload

---
+++ T1059.001-2.ps1
@@ -0,0 +1,6 @@
+﻿#Atomic Test no. 19
+#requires Sharphound
+write-host "Import and Execution of SharpHound.ps1 from C:\Users\studentadmin\Documents\test" -ForegroundColor Cyan
+import-module C:\Users\studentadmin\Documents\test\SharpHound.ps1
+Invoke-BloodHound -OutputDirectory $env:Temp
+Start-Sleep 5
